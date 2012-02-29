63d1ee1c1b9d7a19e6a86a4875bb1e3df0abfdd7
george <rpubaddr0@gmail.com>
2012-02-29 15:07:04 -0700

    committed files

---
+++ cfrac2.c
@@ -0,0 +1,57 @@
+/* cfrac2.c
+ * calculate continued fractions - the second, revised version of cfrac
+ * this adds the ability to accept the terms as arguments, and fixes the 
+ * C syntax errors it had before. It also increases the accuracy of the 
+ * output from %f to %.99g.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  int el = 0, atoi(); /* el = errorlevel */
+  double cfrac(); 
+  void noargs(), rightargs(), usage();
+  if (argc == 3) {
+    int a = atoi(argv[1]), n = atoi(argv[2]);
+    if (n >= 3) {
+      rightargs(a, n);
+    } else {
+      usage();
+    }
+  } else if (argc == 1) {
+    noargs();
+  } else {
+    usage();
+  }
+  return el;
+}
+
+double cfrac(int a, int n) {  /* Feel free to use this function elsewhere if you need it. */
+  int k = 1;
+  double c = a + 1.000 / a;  //This has got to be wrong. I'll look up the formula and see how I can make this better.
+  c = a + 1.000 / c;
+  while(++k < n-1) {
+    c = a + 1.000 / c;
+  }
+  return c;
+}
+
+void noargs() {
+  int a, b;
+  printf("Input term: ");
+  scanf("%d", &a);
+  printf("\nNumber n, n >= 3: ");
+  scanf("%d", &b);
+  printf("\nThe value of that continued fraction is about %.99g\n", cfrac(a, b));
+  return;
+}
+
+void rightargs(int a, int n) {
+  printf("%.99g\n", cfrac(a, n));
+  return;
+}
+
+void usage() {
+  printf("cfrac: calculate Continued FRACtions\nUsage: cfrac [int a] [int n >= 3]\n OR\nUsage: cfrac\t(for interactive mode)\n");
+  return;
+}
---
+++ countby.c
@@ -0,0 +1,18 @@
+/* countby.c
+ * count by n (0 to 100)
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  int n, atoi();
+  if (argc != 2) {
+    printf("usage: %s n\nwhere 'n' is some int to count by, from 0 to 100, inclusive.\n", argv[0]);
+    return 1;
+  }
+  int x = atoi(argv[1]); // this must be after the if, or it will segv when argc > 2
+  for(n = 0; n <= 100; n += x)
+    printf("%d ", n);
+  printf("\n");
+  return 0;
+}
