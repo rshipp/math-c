c0a2601277a1c57cd8575438a0df80eb182e289f
george <rpubaddr0@gmail.com>
2013-01-24 23:54:49 -0700

    update maths.h, add maths.hpp

---
+++ maths.hpp
@@ -0,0 +1,57 @@
+// maths.hpp
+// some arbitrary math functions
+// converted from C to C++
+
+#include <iostream>
+using namespace std;
+
+// count by x, from 0 to e
+void countby(int x, int e)
+{
+    int n;
+
+    for(n = 0; n <= e; n += x)
+        cout << n;
+    cout << endl;
+
+    return;
+}
+
+// calculate continued fractions 
+double cfrac(int a, int n)
+{ 
+    int k = 1;
+    double c = a + 1.000 / a;   // This has got to be wrong.
+
+    c = a + 1.000 / c;          // I'll look up the formula and see
+    while(++k < n-1)            // how I can make this better.
+    {         
+        c = a + 1.000 / c;
+    }
+
+    return c;
+}
+
+// two implementations of a factorial function 
+// inaccurate with 0 > n > 12 because of the int declarations
+int factorial_r(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n * factorial_r(n - 1);
+    }
+}
+
+int factorial_f(int n)
+{
+    for (int i = n - 1; i > 1; i--)
+    {
+        n = n * i;
+    }
+
+    return n;
+}
