c3363a55868560656190c5c5a786209e0ed5f3e0
george2 <rpubaddr0@gmail.com>
2013-06-27 16:02:43 -0600

    more space

--- cfrac2.c
+++ cfrac2.c
@@ -12,6 +12,7 @@ int main(int argc, char *argv[]) {
   int el = 0, atoi(); /* el = errorlevel */
   double cfrac(); 
   void noargs(), rightargs(), usage();
+  
   if (argc == 3) {
     int a = atoi(argv[1]), n = atoi(argv[2]);
     if (n >= 3) {
@@ -24,25 +25,30 @@ int main(int argc, char *argv[]) {
   } else {
     usage();
   }
+  
   return el;
 }
 
 void noargs() {
   int a, b;
+  
   printf("Input term: ");
   scanf("%d", &a);
   printf("\nNumber n, n >= 3: ");
   scanf("%d", &b);
   printf("\nThe value of that continued fraction is about %.99g\n", cfrac(a, b));
+  
   return;
 }
 
 void rightargs(int a, int n) {
   printf("%.99g\n", cfrac(a, n));
+  
   return;
 }
 
 void usage() {
   printf("cfrac: calculate Continued FRACtions\nUsage: cfrac [int a] [int n >= 3]\n OR\nUsage: cfrac\t(for interactive mode)\n");
+  
   return;
 }
