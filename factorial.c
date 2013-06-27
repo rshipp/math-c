519710375e174e374ed4c313f9809c798a521973
george2 <rpubaddr0@gmail.com>
2013-06-27 16:08:18 -0600

    else usage

--- factorial.c
+++ factorial.c
@@ -1,4 +1,4 @@
-/* just a demo main() for the factorial funcs
+/* just a demo main() for the factorial functions
  */
 
 #include <stdio.h>
@@ -10,6 +10,9 @@ int main(int argc, char *argv[]) {
   if (argc >= 2) { /* prevent seg fault */
     printf("rec: %d\n", factorial_r(atoi(argv[1])));
     printf("for: %d\n", factorial_f(atoi(argv[1])));
+  } else {
+    printf("usage: %s integer\n", argv[0]);
+    printf("where 0 < integer < 12\n");
   }
   
   return 0;
