a9ae3564cd9e9c888888b953ab1f4ec89c5dd789
george2 <rpubaddr0@gmail.com>
2013-06-27 16:04:48 -0600

    more spaces!

--- factorial.c
+++ factorial.c
@@ -6,9 +6,11 @@
 
 int main(int argc, char *argv[]) {
   int atoi();
+  
   if (argc >= 2) { /* prevent seg fault */
     printf("rec: %d\n", factorial_r(atoi(argv[1])));
     printf("for: %d\n", factorial_f(atoi(argv[1])));
   }
+  
   return 0;
 }
